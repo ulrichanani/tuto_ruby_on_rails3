7fce55b81129a3dd951799604244fc3af0d05c5a
Ulrich Anani <ulrichanan@gmail.com>
2018-09-28 09:45:59 +0100

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require File.expand_path('../boot', __FILE__)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,5 +17,8 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
